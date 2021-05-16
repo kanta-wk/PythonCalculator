05a3e8b0db4c194b7a06979da23d7643d177182d
RikitoNoto <rikitonoto@gmail.com>
2021-05-16 17:20:35 +0900

    write the comment to test_Calculator.py

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -7,8 +7,8 @@ from src.Calculator import Calculator
 
 
 class CalculatorTest(unittest.TestCase):
-    LEFT_VALUE = 4564
-    RIGHT_VALUE = 349
+    LEFT_VALUE = 4564#テスト用の左辺
+    RIGHT_VALUE = 349#テスト用の右辺
 
     def setUp(self) -> None:
         self.create_calculator(self.LEFT_VALUE)
@@ -19,54 +19,99 @@ class CalculatorTest(unittest.TestCase):
         pass
 
     def create_calculator(self, left_value):
+        """
+        calculatorインスタンスを作成し、インスタンス変数へ代入する
+        :param left_value: 左辺
+        :return:
+        """
         self.calculator = Calculator(left_value)
 
     @unittest.skip("write the code")
     def test_add(self):
+        """
+        加算のテスト。
+        :return:
+        """
         self.calculator.operator#TODO 加算のオペレータを入力する処理
         self.assertEqual(self.calculator.calculate(), self.LEFT_VALUE + self.RIGHT_VALUE)
 
     @unittest.skip("write the code")
     def test_sub(self):
+        """
+        減算のテスト
+        :return:
+        """
         self.calculator.operator#TODO 減算のオペレータを入力する処理
         self.assertEqual(self.calculator.calculate(), self.LEFT_VALUE - self.RIGHT_VALUE)
 
     @unittest.skip("write the code")
     def test_multi(self):
+        """
+        掛け算のテスト
+        :return:
+        """
         self.calculator.operator#TODO かけ算のオペレータを入力する処理
         self.assertEqual(self.calculator.calculate(), self.LEFT_VALUE * self.RIGHT_VALUE)
 
     @unittest.skip("write the code")
     def test_divi(self):
+        """
+        割り算のテスト
+        :return:
+        """
         self.calculator.operator#TODO 割り算のオペレータを入力する処理
         self.assertEqual(self.calculator.calculate(), self.LEFT_VALUE / self.RIGHT_VALUE)
 
     @unittest.skip("write the code")
     def test_formula_add(self):
+        """
+        足し算後の式が正しく受け取れるかのテスト
+        :return:
+        """
         self.test_add()
         self.assertEqual(self.calculator.formula, "{}+{}".format(self.LEFT_VALUE, self.RIGHT_VALUE))
 
     @unittest.skip("write the code")
     def test_formula_sub(self):
+        """
+        減算後の式が正しく受け取れるかのテスト
+        :return:
+        """
         self.test_sub()
         self.assertEqual(self.calculator.formula, "{}-{}".format(self.LEFT_VALUE, self.RIGHT_VALUE))
 
     @unittest.skip("write the code")
     def test_formula_multi(self):
+        """
+        掛け算後の式が正しく受け取れるかのテスト
+        :return:
+        """
         self.test_multi()
         self.assertEqual(self.calculator.formula, "{}×{}".format(self.LEFT_VALUE, self.RIGHT_VALUE))
 
     @unittest.skip("write the code")
     def test_formula_divi(self):
+        """
+        割り算後の式が正しく受け取れるかのテスト
+        :return:
+        """
         self.test_divi()
         self.assertEqual(self.calculator.formula, "{}÷{}".format(self.LEFT_VALUE, self.RIGHT_VALUE))
 
     @unittest.skip("write the code")
     def test_left_value(self):
+        """
+        左辺の代入が正しくできるかのテスト
+        :return:
+        """
         self.assertEqual(self.calculator.left_value, self.LEFT_VALUE)
 
     @unittest.skip("write the code")
     def test_right_value(self):
+        """
+        右辺の代入が正しくできるかのテスト
+        :return:
+        """
         self.calculator.right_value = self.RIGHT_VALUE
         self.assertEqual(self.calculator.right_value, self.RIGHT_VALUE)
 
