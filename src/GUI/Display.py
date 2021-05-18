34cf9a39c736973ad2f104baab37f9d5dc75b40f
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2021-05-18 23:24:12 +0900

    correct mistakes and tuning something

--- src/GUI/Display.py
+++ src/GUI/Display.py
@@ -1,10 +1,11 @@
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty
 
 class Display(Widget):
     Builder.load_file("./Display.kv")
     text = StringProperty("")
+    font_size = NumericProperty(50)
 
 if __name__ == '__main__':
     from kivy.app import App
